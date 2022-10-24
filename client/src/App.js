f4ddb7ad7564a6f048ef92ccdc2c7d0fa497a4b9
Ed <105404937+indy6678@users.noreply.github.com>
2022-10-23 21:26:20 -0500

    added utils and query.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import {ApolloProvider, ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
 import React from 'react';
 
 import Header from './components/Header';
@@ -5,8 +6,22 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+// establish new link to GraphQL server at its /graphql endpoint
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+// instantiate the Apollo client instance and create the connection to the API endpoint (will accept options)
+// instantiate a new cache object using new InMemoryCache() (customizable to app)
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    // wrap entire JSX
+    <ApolloProvider client={client}>
     <div className='flex-column justify-flex-start min-100-vh'>
       <Header />
       <div className='container'>
@@ -14,6 +29,7 @@ function App() {
       </div>
       <Footer />
     </div>
+    </ApolloProvider>
   );
 }
 
