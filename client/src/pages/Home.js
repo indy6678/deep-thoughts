e8e4b247fd3b875da6eaf51dd73ec83135261f9c
Ed <105404937+indy6678@users.noreply.github.com>
2022-10-23 21:54:48 -0500

    modified home component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,16 @@
 import React from 'react';
+// import useQuery hook, to make requests to GraphQL server
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';
 
 const Home = () => {
+
+  // use useQuery hook to make query request upon home component load (async)
+  const {loading, data}= useQuery(QUERY_THOUGHTS);
+  // optional chaining; if data exists, store it in the thoughts constant we just created. If data is undefined, then save an empty array to the thoughts component.
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+
   return (
     <main>
       <div className='flex-row justify-space-between'>
