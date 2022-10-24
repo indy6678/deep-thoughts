eaf479d919ad368cdf0d36a746e205d04cf56027
Ed <105404937+indy6678@users.noreply.github.com>
2022-10-23 22:15:44 -0500

    added thoughtlist

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import useQuery hook, to make requests to GraphQL server
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHTS } from '../utils/queries';
+import ThoughtList from '../components/ThoughtList';
 
 const Home = () => {
 
@@ -14,7 +15,13 @@ const Home = () => {
   return (
     <main>
       <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+        <div className='col-12 mb-3'>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+          )}
+          </div>
       </div>
     </main>
   );
